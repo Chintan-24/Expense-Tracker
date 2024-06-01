c47b38639415312bdcc2bcb6b489d14c85946875
Chintan Bhalodiya <chintanbhalodiya24@gmail.com>
2024-06-02 03:19:57 +0530

    initial commit

---
+++ src/test/java/com/project/Expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
